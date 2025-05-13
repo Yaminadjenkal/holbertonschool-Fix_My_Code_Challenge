f567ae7f8c61be186420916968750d1263da2f6d
Yaminadjenkal <mina20101951@hotmail.com>
2025-05-13 10:48:01 +0100

    first task

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fizzbuzz(n):
+    """Affiche les nombres de 1 à n en remplaçant les multiples de 3 et 5."""
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
+
+# Appel de la fonction avec 50
+fizzbuzz(50)
